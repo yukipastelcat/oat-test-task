555d9f6d2f7bda91d0cd7e667eaaf2b5cd6f37c5
Kirill Sedunov <kirill.sedunov@outlook.com>
2021-05-04 09:17:49 +0300

    chore: Provided an example of using list component with other kinds of data

--- src/main.js
+++ src/main.js
@@ -1,6 +1,18 @@
 import 'core-js/es7'
 import 'regenerator-runtime/runtime'
 import UsersList from './components/users-list'
+import List from './components/list'
 
 const app = new UsersList()
 app.mount(document.querySelector('#app'))
+
+const anyDataList = new List(
+  {
+    items: [...Array(100).keys()].map((item) => ({ name: `Item ${item}` }))
+  },
+  {
+    listItem: (item) => item.name
+  }
+)
+anyDataList.mount(document.querySelector('#any-data-example'))
+
